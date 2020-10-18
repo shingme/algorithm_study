90673f6d97d419b07a3d828b2e6f5eeb5a638f92
shingme <sinmi1620@gmail.com>
2020-10-18 23:42:25 +0900

    최적화해서 풀기

--- src/baekjoon/baekjoon_11053.java
+++ src/baekjoon/baekjoon_11053.java
@@ -1,7 +1,5 @@
 package baekjoon;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 //반례가 존재함
 public class baekjoon_11053 {
@@ -10,32 +8,28 @@ public class baekjoon_11053 {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
-		int[] A = new int[n];
-		List<Integer> resultA;
-		int MaxLen = 0;
-		int z = 0;
+		int[] A = new int[n]; // 10,20,10,30,20,50
+		int[] DP = new int[n];
+		
 		for(int i=0; i<n; i++){
 			A[i] = input.nextInt();
 		}
 		
-		for(int i=0; i<n-1; i++){
-			z = i;
-			int cnt = 0;
-			resultA = new ArrayList<Integer>();
-			resultA.add(cnt++, A[i]);
-			for(int j=i+1; j<n; j++){
-				if(A[i] < A[j]){
-					resultA.add(cnt++, A[j]);
-					i=j;
+		DP[0] = 1;
+		for(int i=1; i<n; i++){
+			DP[i] = 1;
+			
+			for(int j=0; j<i; j++){
+				if(A[j] < A[i] && DP[i] <= DP[j]){
+					DP[i] = DP[j]+1;
 				}
 			}
-			if(MaxLen < resultA.size()){
-				MaxLen = resultA.size();
-				i = z;
-			}
 		}
-		
-		System.out.println(MaxLen);
+		int max = 0;
+		for(int num : DP){
+			max = Math.max(max, num);
+		}
+		System.out.println(max);
 	}
 
 }
